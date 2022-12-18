ae33d19cd5941728fb60d4c3553de9fd6ee758be
Misbah Khalil <misbah.khalil.az@gmail.com>
2022-12-18 15:28:30 +0500

    feat: online users indicator

--- src/webSocketServer.ts
+++ src/webSocketServer.ts
@@ -4,6 +4,7 @@ import { MessagePayload } from "./types/message";
 import { authWebSocket } from "./middlewares/authWebSocket.middleware";
 import { sendMessage } from "./controllers/socket.controller";
 import { decodeToken } from "./utils/secret.util";
+import { User } from "./models";
 
 export const mountWebSocketServer = (
   server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>
@@ -18,14 +19,22 @@ export const mountWebSocketServer = (
 
   io.use(authWebSocket);
 
-  io.on("connection", (socket) => {
+  io.on("connection", async (socket) => {
     const { userName } = decodeToken(socket.handshake.query.token as string);
+
+    await User.findOneAndUpdate({ userName }, { isOnline: true });
+
+    socket.broadcast.emit("online-user", userName);
+
     socket.join(`chatroom:${userName}`);
 
     socket.on("send-message", (message: MessagePayload) =>
       sendMessage(socket, message)
     );
 
-    socket.on("disconnect", () => {});
+    socket.on("disconnect", async () => {
+      await User.findOneAndUpdate({ userName }, { isOnline: false });
+      socket.broadcast.emit("offline-user", userName);
+    });
   });
 };
